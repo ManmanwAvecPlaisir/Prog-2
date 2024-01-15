d1b9d68a77bdb537385f2aa23ad98b1c0c444d9c
ManmanwAvecPlaisir <79599441+ManmanwAvecPlaisir@users.noreply.github.com>
2024-01-15 15:59:58 -0400

    Add files via upload

---
+++ fichier.js
@@ -0,0 +1,4 @@
+document.title="Bonjour!";
+document.body.setAttribute("text","grey");
+document.body.querySelector("span").innerHTML=" Mathématique et Informatique";
+onload = alert(" bienvenu sur ma page");
